67b769b3416ea27470264bb8639056a33a3fb835
Gabriel Tindall <m1240613@moreheadstate.edu>
2023-04-06 19:08:07 -0400

    saving

---
+++ index.js
@@ -0,0 +1,74 @@
+
+if(localStorage.getItem(1234) == null){
+	let gamestate = {
+		money : 0,
+		research : 0,
+		power : 0,
+		power_cap : 200,
+		total_time : 0,
+		powerPerSecond : 12,
+	}
+	console.log("gamestate is null");
+	console.log(gamestate.power)
+} else {load();}
+
+function save(gamestate){
+	localStorage.setItem(1234, JSON.stringify(gamestate));
+	console.log("boobs")
+}
+
+function load() {
+	if (localStorage.getItem(1234) != null){
+		console.log("ass")
+		gamestate = JSON.parse(localStorage.getItem(1234));
+		updateGame(0, gamestate.total_time)
+		console.log("ass", gamestate.total_time)
+}}
+
+function highlightOn(obj){
+	obj.style = "background:grey";
+}
+
+function highlightOff(obj){
+	obj.style = "background:black";
+}
+
+function getTile(){
+	tile = document.querySelectorAll('[data-pos="1_34"]');
+	tile.style = "background:blue";
+	console.log(tile)
+}
+
+let last_time = null;
+setInterval(function GameLoop(){
+	const current_time = Date.now();
+	if (last_time === null){
+		last_time = current_time;}
+	const delta_time = current_time - last_time;
+	gamestate.total_time += delta_time;
+	last_time = current_time;
+	updateGame(delta_time, gamestate.total_time)
+	updatePowerbar()
+},1000/60)
+
+function updateGame(delta_time, total_time){
+	let powerPerMillisecond = gamestate.powerPerSecond/1000;
+	if (gamestate.power < gamestate.power_cap){
+	gamestate.power += powerPerMillisecond*delta_time;
+	}
+	document.getElementById("powerPossessed").innerHTML = gamestate.power.toFixed(2);
+	document.getElementById("powerIncome").innerHTML = `+${gamestate.powerPerSecond}`;
+	document.getElementById("maxpowerPossessed").innerHTML = gamestate.power_cap;
+	document.getElementById("moneyPossessed").innerHTML = gamestate.money.toFixed(2)
+}
+
+function updatePowerbar(){
+	powerBar = document.getElementById("powerbarFill")
+	powerBar.style.width = `${(gamestate.power*100)/gamestate.power_cap}%`
+}
+
+function sellPower(){
+	gamestate.money += gamestate.power;
+	gamestate.power = 0;
+	console.log("hey")
+}
--- research.js
+++ research.js
@@ -1,15 +1,13 @@
 let ReactorTab = document.getElementById("ReactorTab")
-let ResearchTab = document.getElementById("ResearchTab")
+let OverviewTab = document.getElementById("OverviewTab")
 let UpgradesTab = document.getElementById("UpgradesTab")
 let SellButton = document.getElementById("sellButton")
 let SaveButton = document.getElementById("saveButton")
 let LoadButton = document.getElementById("loadButton")
 ReactorTab.addEventListener("mouseover",function(){highlightOn(ReactorTab);});
 ReactorTab.addEventListener("mouseout",function(){highlightOff(ReactorTab);});
-ResearchTab.addEventListener("mouseover",function(){highlightOn(ResearchTab);});
-ResearchTab.addEventListener("mouseout",function(){highlightOff(ResearchTab);});
+OverviewTab.addEventListener("mouseover",function(){highlightOn(OverviewTab);});
+OverviewTab.addEventListener("mouseout",function(){highlightOff(OverviewTab);});
 UpgradesTab.addEventListener("mouseover",function(){highlightOn(UpgradesTab);});
 UpgradesTab.addEventListener("mouseout",function(){highlightOff(UpgradesTab);});
 SellButton.addEventListener("click",function(){sellPower();});
-SaveButton.addEventListener("click",function(){save(gamestate);});
-LoadButton.addEventListener("click",function(){load();});
